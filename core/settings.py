732948f7a2e06db3c235a44f35c8998b53699f24
lokes <lokeswargahir349@gmail.com>
2024-06-26 13:42:21 +0530

    setting debug=false

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mbqsyo@y21b@9b*vqzkmb2ubg7%bl1w2u23$6^d)jd=b^+x4g6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.0', 'studentapi-htla.onrender.com']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'my_files')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
